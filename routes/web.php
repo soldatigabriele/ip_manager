00ab1ed58d6a7671a61a71b8cf19fccbefe9e24e
Gabriele Soldati <soldati.gabriele@gmail.com>
2019-06-30 00:30:49 +0100

    add timestamp

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,21 @@
 <?php
 
 $router->get('/ip/latest', function () use ($router) {
-    return response(['address' => \App\Ip::latest()->first()->address]);
+    $ip = \App\Ip::latest('id')->first();
+    return response([
+        'address' => $ip->address,
+        'date' => $ip->created_at,
+    ]);
+});
+
+$router->get('/ip/all', function () use ($router) {
+    $ips = \App\Ip::latest('id')->limit(15)->get();
+    return response(
+        $ips->toArray()
+    );
 });
 
 $router->get('/ip/register', function () use ($router) {
     $ip = \App\Ip::create(['address' => \Illuminate\Support\Facades\Request::ip()]);
     return response($ip->toArray(), 201);
-    // return response(['address' => \App\Ip::latest()->first()->address]);
 });
--- tests/IpsTest.php
+++ tests/IpsTest.php
@@ -16,6 +16,7 @@ class IpsTest extends TestCase
         $response = $this->get('/ip/register');
         $this->assertEquals(201, $response->response->getStatusCode());
         $this->assertEquals('127.0.0.1', json_decode($response->response->getContent())->address);
+        $this->assertEquals(Carbon::now(), json_decode($response->response->getContent())->created_at);
     }
 
     /**
@@ -30,5 +31,22 @@ class IpsTest extends TestCase
         $response = $this->get('/ip/latest');
         $address = json_decode($response->response->getContent(), true);
         $this->assertEquals($ipEntry->address, $address['address']);
+        $this->assertNotNull(json_decode($response->response->getContent())->date);
+    }
+
+    /**
+     * @return void
+     */
+    public function testIndex()
+    {
+        Ip::unguard();
+        for ($i=0; $i < 20; $i++) { 
+            Ip::create(['address' => '12.21.12.21']);
+        }
+        $latest = Ip::create(['address' => '19.31.12.32']);
+
+        $response = $this->get('/ip/all');
+        $this->assertEquals(200, $response->response->getStatusCode());
+        $this->assertContains($latest->address, $response->response->getContent());
     }
 }
